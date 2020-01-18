b6f881e22987653cacf23ff48bb5827602abd37e
Dmitry Pavlov <pvlv@mail.ru>
2020-01-18 16:38:50 +0500

    Initial Commit

---
+++ InSat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InSat
+//
+//  Created by Dmitry Pavlov on 18.01.2020.
+//  Copyright © 2020 Dmitry Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
